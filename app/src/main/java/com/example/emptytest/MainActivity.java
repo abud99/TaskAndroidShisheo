eaea4bf928b3645a832c4682fae19a0c2ce8835e
abud99 <57545385+abud99@users.noreply.github.com>
2021-08-17 23:17:01 +0400

    RxJava not sure how to make it run on UI thread

--- app/src/main/java/com/example/emptytest/MainActivity.java
+++ app/src/main/java/com/example/emptytest/MainActivity.java
@@ -16,6 +16,12 @@ import org.json.JSONException;
 
 import java.io.IOException;
 
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.OkHttpClient;
@@ -27,6 +33,10 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView view;
     private RecyclerView.Adapter adapter;
     private RecyclerView.LayoutManager layoutManager;
+    private final String url = "https://gateway-dev.shisheo.com/social/api/web/post/arina/test";
+    private Request request;
+    private Observable<String> observable;
+    private Observer<String> observer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,47 +46,65 @@ public class MainActivity extends AppCompatActivity {
         view = findViewById(R.id.recyclerView);
         layoutManager = new LinearLayoutManager(this);
         view.setHasFixedSize(true);
+        observable = Observable.just("notsure");
         view.setLayoutManager(layoutManager);
+        observable.observeOn(AndroidSchedulers.mainThread());
+        observable.subscribeOn(Schedulers.io());
 
 
+         observer = new Observer<String>() {
+             @Override
+             public void onSubscribe(@NonNull Disposable d) {
+                 request = new Request.Builder()
+                         .url(url)
+                         .build();
+             }
 
-        String url = "https://gateway-dev.shisheo.com/social/api/web/post/arina/test";
+             @Override
+             public void onNext(@NonNull String s) {
 
-        Request request = new Request.Builder()
-                .url(url)
-                .build();
+             }
 
-        client.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-                e.printStackTrace();
-            }
+             @Override
+             public void onError(@NonNull Throwable e) {
 
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                if (response.isSuccessful()) {
-                    final String myResponse = response.body().string();
+             }
 
-                    MainActivity.this.runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            try {
-                                 parse = new Parser(myResponse);
-                                adapter = new CustomAdapter(Parser.list);
-                                view.setAdapter(adapter);
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
+             @Override
+             public void onComplete() {
+                 client.newCall(request).enqueue(new Callback() {
+                     @Override
+                     public void onFailure(@androidx.annotation.NonNull Call call, @androidx.annotation.NonNull IOException e) {
+
+                     }
+
+                     @Override
+                     public void onResponse(@androidx.annotation.NonNull Call call, @androidx.annotation.NonNull Response response) throws IOException {
+                        if(response.isSuccessful()){
+                            MainActivity.this.runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    try {
+                                        parse = new Parser(response.body().string());
+                                        adapter = new CustomAdapter(Parser.list);
+                                        view.setAdapter(adapter);
+                                        adapter.notifyDataSetChanged();
+                                    } catch (JSONException | IOException e) {
+                                        e.printStackTrace();
+                                    }
+
+                                }
+                            });
+
 
                         }
-                    });
-                }
-                else{
-                    System.out.println("Something went wrong?");
-                }
+                     }
+                 });
+             }
+         };
 
-            }
-        });
+        observable.subscribe(observer);
+
 
 
 
